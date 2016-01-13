4df381b761c19497fc5ae54949bf4dff3ad29e24
morenz75 <moreno.riccardi@gmail.com>
2016-01-13 11:58:13 +0100

    first commit
    First commit of the shinyaps application

---
+++ code/shiny/server.R
@@ -0,0 +1,44 @@
+library(shiny)
+load("final.Rdata")
+
+predict <- function(sentence) {
+    # given a sentence/phrase, extract the last two words
+    sl <- unlist(str_split(sentence," "))
+    sl <- gsub("[^a-zA-zèòàùèéùì]|\\[|\\]|\\\\|_|\\^|-|`", "", sl)
+    sl <- sl[which(sl != "")]
+    len <- length(sl)
+    bigram <- paste(sl[len-1],sl[len])
+    predictions <- vector()
+    # get the subset of the trigram data table with a matching bigram start
+    #swf_T <- wf_T[wf_T$start == bigram,]
+    nextWords <- df_b[row.names(df_b) == bigram,]
+    #check if bigram was found in the trigram table
+    if(nrow(nextWords) > 0) {
+        return(unlist(str_split(nextWords$topNextWord,",")))
+    } else {
+        #print(paste("No match for bigram",bigram,"in",sentence,"--looking for unigram match"))
+        unigram <- sl[len]
+        nextWords <- df_u[row.names(df_u) == unigram,]
+        if(nrow(nextWords) > 0) {
+            return(unlist(str_split(nextWords$topNextWord,",")))
+        } else {
+            return(head(row.names(df_u),3))
+        }
+    }   
+}
+
+# Define server logic required to draw a histogram
+shinyServer(function(input, output) {
+    
+    output$predictWord <- renderUI({
+        #output$distPlot <- renderDataTable({
+        if (input$action==0) return(NULL)
+        
+        isolate({
+            HTML(paste("<ul><li>",
+                paste(predict(input$phrase), collapse = "</li><li>", sep = ""),
+                "</li></ul>", sep="")
+            )
+        })
+    })
+})
---
+++ code/shiny/ui.R
@@ -0,0 +1,26 @@
+library(shiny)
+
+# Define UI for application that draws a histogram
+shinyUI(fluidPage(
+    
+    # Application title
+    titlePanel("DSC - Swiftkey Capstone"),
+    
+    mainPanel(
+        fluidRow(
+            h4("Please enter your phrase and hit \"Predict next word\" button"),
+            textInput("phrase", 
+                      label = h3("Your phrase"), 
+                      value = "Enter text...", 
+                      width = "1200"),
+            br(),
+            actionButton("action", label = "Predict next word"),
+            br(),br(),
+            conditionalPanel(
+                condition = "input.action != 0",
+                htmlOutput("predictWord")
+            )
+        )
+    )
+    )
+)
